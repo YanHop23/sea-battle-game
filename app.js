f8c8a1c2cf72f8d34c90aeaafb85f2845dd96c30
Oleg Polothevets <olegpolothevets@gmail.com>
2024-01-28 01:36:46 +0200

    generation area

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("lol");
+const areaYou = document.querySelector(".game__area-you");
+const areaBot = document.querySelector(".game__area-bot");
+const startBtn = document.querySelector(".startbtn");
+
+const rows = ["1", "2","3","4","5","6","7","8","9","10"];
+const colums = ["A", "B","C","D","E","F","G","H","I","J"];
+
+
+
+startBtn.addEventListener('click', () => {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            areaYou.innerHTML += `<div class="y${rows[i] + colums[j]} cell">0</div>`;
+            areaBot.innerHTML += `<div class="b${rows[i] + colums[j]} cell">x</div>`;
+        };
+    };
+});
